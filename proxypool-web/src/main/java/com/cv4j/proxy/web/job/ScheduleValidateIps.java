6ef383aa7a406c9600ae672570b935a65fd2044a
morrislchg <a18737170569>
2018-01-18 16:07:36 +0800

    添加db脚本

---
+++ proxypool-web/src/main/java/com/cv4j/proxy/web/job/ScheduleValidateIps.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (C), 2017-2018, lc
+ * FileName: ScheduleValidateIps
+ * Author:   carlson
+ * Date:     2018/1/18 0018 16:03
+ * Description: 更新ip评分任务
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.cv4j.proxy.web.job;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈更新ip评分任务〉
+ *
+ * @author carlson
+ * @create 2018/1/18 0018
+ * @since 1.0.0
+ */
+public class ScheduleValidateIps {
+}
